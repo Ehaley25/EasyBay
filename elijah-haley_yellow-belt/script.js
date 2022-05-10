2ec15933a90aa22c44ae014bc86c984c8ba9a37e
Elijah Haley <elijahhaley99@gmail.com>
2022-05-09 17:57:52 -0700

    bad modularization

--- elijah-haley_yellow-belt/script.js
+++ elijah-haley_yellow-belt/script.js
@@ -1,9 +1,9 @@
 function change(_id){
-    _id.src="succulents-2.jpg";
+    _id.src="images/succulents-2.jpg";
 }
 
 function changeBack(_id){
-    _id.src="succulents-1.jpg";
+    _id.src="images/succulents-1.jpg";
 }
 
 function disappear(_id){
