21c041532b690a005a3bc433ddd6ff67239b8448
Elijah Haley <elijahhaley99@gmail.com>
2022-05-09 17:37:33 -0700

    first commit

---
+++ elijah-haley_yellow-belt/script.js
@@ -0,0 +1,11 @@
+function change(_id){
+    _id.src="succulents-2.jpg";
+}
+
+function changeBack(_id){
+    _id.src="succulents-1.jpg";
+}
+
+function disappear(_id){
+    document.querySelector(_id).remove();
+}
